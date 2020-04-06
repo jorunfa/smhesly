abae93fddf14ceb49e89caed018f2c586a6d5bb8
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2020-04-06 15:48:33 +0200

    Use clap for CLI app

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,46 @@
-use std::env;
+extern crate clap;
+
+use clap::{App, Arg};
 
 fn main() {
-    let from = env::var("FROM").unwrap();
-    let to = env::var("TO").unwrap();
-    let content = env::var("CONTENT").unwrap();
+    let matches = App::new("Smhesly")
+        .version("0.1")
+        .about("Send SMS using WGTWO API")
+        .arg(
+            Arg::with_name("from")
+                .short("f")
+                .long("from")
+                .value_name("FROM")
+                .help("Sender of SMS")
+                .takes_value(true)
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("to")
+                .short("t")
+                .long("to")
+                .value_name("TO")
+                .help("Receiver of SMS")
+                .takes_value(true)
+                .required(true)
+                .index(2),
+        )
+        .arg(
+            Arg::with_name("message")
+                .short("m")
+                .long("message")
+                .value_name("MESSAGE")
+                .help("SMS body content")
+                .takes_value(true)
+                .required(true)
+                .index(3),
+        )
+        .get_matches();
+
+    let from = matches.value_of("from").unwrap().to_owned();
+    let to = matches.value_of("to").unwrap().to_owned();
+    let content = matches.value_of("message").unwrap().to_owned();
 
     smhesly::send_sms(from, to, content).expect("Failed to send SMS");
 }
